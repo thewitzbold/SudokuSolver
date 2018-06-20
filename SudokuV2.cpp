f83210b7fcdf6e8c3d4b78b704e3a76bdb6362bf
thewitzbold <edenbernabe@gmail.com>
2018-06-20 00:00:37 -0700

    clean sudoku - backtracking

--- SudokuV2.cpp
+++ SudokuV2.cpp
@@ -1,189 +1,131 @@
-// Home
+/*
+	0  1  2    3  4  5    6  7  8
+	9  10 11   12 13 14   15 16 17
+	18 19 20   21 22 23   24 25 26
+
+	27 28 29   30 31 32   33 34 35
+	36 37 38   39 40 41   42 43 44
+	45 46 47   48 49 50   51 52 53
+
+	54 55 56   57 58 59   60 61 62
+	63 64 65   66 67 68   69 70 71
+	72 73 74   75 76 77   78 79 80
+*/
 
 #include <iostream>
 
-using namespace std;
+using std::cout;
+using std::endl;
 
-class Square
+const int SIZE = 81;
+const int ROW_COL = 9;
+
+void print(int* input)
 {
-public:
-  int num;
-  bool visited;g++
-  bool constant; //From input
-};
+	cout << endl;
+	cout << "--------------------------" << endl;
+	for(int index = 0; index < SIZE; index++){
+		if(index % ROW_COL == 0 && index != 0)
+			cout << endl;
+		if(index % (ROW_COL*3) == 0 && index != 0)
+			cout << endl;
+		cout << input[index] << " ";
+	}
+	cout << endl;
+}
 
-void print();
-bool check(int, int);
-bool Backtracking(int, int);
+bool check(int* input,int position){
 
-Square box[9][9];
-int row, col;
-bool direction;// 1 -> forward, 0 -> backwards
+	//Check row & col at the same time
+	int col = position % ROW_COL;
+	int row = position - col;
+	int count = 0;
+	for(int i=0; i<ROW_COL ;col += 9, row++, i++)
+	{
+		if(input[col] == input[position])
+			count++;
+		if(input[row] == input[position])
+			count++;
+		if(count >= 3)
+			return false;
+	}
+
+	//Check for 3x3 square
+	int temp_position = position;
+	while(true)
+	{
+		col = temp_position % ROW_COL;
+		if(col % 3 == 0 )
+			break;
+		else
+			--temp_position;
+	}
+
+	while(true)
+	{
+		if(position >= col && position < (col+27) )
+			break;
+		else
+			col = col + 27;
+	}
+
+	int pos = col;
+	count = 0;
+	for(int i=0; i<3; i++) // 3 times
+	{
+		--pos;
+		for(int j=0; j<3; j++) // 3 times
+		{
+			pos = pos+1;
+			if(input[pos] == input[position])
+				count++;
+			if(count >= 2)
+				return false;
+		}
+		pos = pos + 9 - 2;
+	}
+
+	return true;
+}
+
+bool backtracking(int* input, int position)
+{
+	if( position == SIZE)
+		return true;
+
+	if(input[position] == 0)
+	{
+		while( input[position] != 9 )
+		{
+			input[position] += 1;
+			if( !check(input, position) )
+				continue;
+			if( backtracking(input, position + 1) )
+				return true;
+		}
+	}
+	else
+		return backtracking(input, position + 1);
+
+	input[position] = 0;
+	return false;
+}
 
 int main()
 {
-  int input[9][9] = {   {8,0,0, 0,0,0, 0,0,0},
-                        {0,0,3, 6,0,0, 0,0,0},
-                        {0,7,0, 0,9,0, 2,0,0},
+	// A soduko unsolved
+	int input[SIZE] = {		8,0,0, 0,0,0, 0,0,0,
+							0,0,3, 6,0,0, 0,0,0,
+							0,7,0, 0,9,0, 2,0,0,
 
-                        {0,5,0, 0,0,7, 0,0,0},
-                        {0,0,0, 0,4,5, 7,0,0},
-                        {0,0,0, 1,0,0, 0,3,0},
+							0,5,0, 0,0,7, 0,0,0,
+							0,0,0, 0,4,5, 7,0,0,
+							0,0,0, 1,0,0, 0,3,0,
 
-                        {0,0,1, 0,0,0, 0,6,8},
-                        {0,0,8, 5,0,0, 0,1,0},
-                        {0,9,0, 0,0,0, 4,0,0}, };
-
-  for (row = 0; row < 9; row++)
-    for (col = 0; col < 9; col++)
-    {
-      Square temp;
-      if(input[row][col] == 0)
-      {
-        temp.visited = false;
-        temp.constant = false;
-      }
-      else
-      {
-        temp.visited = true;
-        temp.constant = true;
-        temp.num = input[row][col];
-      }
-      box[row][col] = temp;
-    }
-
-  //Done input, ready to solve it.
-  direction = true;
-  print();
-  if ( Backtracking(0,0) )
-    print();
-  else
-    cout << "Impossible to solve it..." << endl;
-}
-
-void print()
-{
-  cout << "----------------------" << endl;
-
-  for (row = 0; row < 9; row++)
-  {
-    if(row%3 == 0) cout << endl;
-    for (col = 0; col < 9; col++)
-    {
-        if(col%3 == 0) cout << " ";
-        if(box[row][col].visited == true)
-          cout << box[row][col].num;
-        else
-          cout << " ";
-    }
-    cout << endl;
-  }
-}
-
-bool Backtracking(int r, int c)
-{
-  if(r == 9) return true;
-
-  if(box[r][c].constant == true)
-  {
-    if(direction == true)
-    {
-      if(c==8)
-      {
-        c = -1;
-        r++;
-      }
-      return Backtracking(r,c+1);
-    }
-    else
-    {
-      if(c==0)
-      {
-        c = 9;
-        r--;
-      }
-      return Backtracking(r,c-1);
-    }
-  }
-
-  box[r][c].num = 0;
-  box[r][c].visited = true;
-
-  while(true)
-  {
-    box[r][c].num++;
-
-    if(box[r][c].num == 10)
-    {
-      box[r][c].visited = false;
-      return false;
-    }
-
-    if( check(r,c) )
-    {
-      direction = true;
-      if(c==8)
-      {
-        c = -1;
-        r = r+1;
-      }
-
-      if ( Backtracking(r,c+1) )
-      {
-        direction = false;
-        return true;
-      }
-      else
-        direction = false;
-    }
-  }
-}
-
-bool check(int r, int c)
-{
-  //Check row
-  for(int i=0; i < 9; i++)
-  {
-    if(i == c) continue;
-
-    if(box[r][i].visited == true)
-      if(box[r][i].num == box[r][c].num)
-        return false;
-  }
-
-  //Check col
-  for(int i=0; i < 9; i++)
-  {
-    if(i == r) continue;
-
-    if(box[i][c].visited == true)
-      if(box[i][c].num == box[r][c].num)
-        return false;
-  }
-
-  //Check 3x3 box
-  int temp_r = r;
-  int temp_c = c;
-
-  while(true)
-  {
-    if(temp_r % 3 != 0) temp_r--;
-    else break;
-  }
-  while(true)
-  {
-    if(temp_c % 3 != 0) temp_c--;
-    else break;
-  }
-
-  for(int i=0; i<3; i++)
-    for(int j=0; j<3; j++)
-    {
-      if(temp_r+i == r && temp_c+j == c) continue;
-
-      if(box[temp_r+i][temp_c+j].visited == true)
-        if(box[temp_r+i][temp_c+j].num == box[r][c].num)
-          return false;
-    }
-  return true;
+							0,0,1, 0,0,0, 0,6,8,
+							0,0,8, 5,0,0, 0,1,0,
+							0,9,0, 0,0,0, 4,0,0, };
+	print(input);
+	backtracking(input, 0);
+	print(input);
 }
